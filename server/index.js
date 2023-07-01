5d80fbca88bbab9eb45ca1aa63d20ace489a7c44
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-07-01 12:41:52 +0200

    fix cors error

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const { calculator, OPERATIONS } = require('./calculator');
