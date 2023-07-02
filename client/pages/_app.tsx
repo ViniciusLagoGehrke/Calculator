6b45fad0dcb693cae88f97b526391ed8d596f8cc
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-07-02 18:03:25 +0200

    add new font

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from 'next/font/local' 
 
-import localFont from 'next/font/local'
-const digital = localFont({ src: '../public/fonts/Digital7-rg1mL.ttf'})
+const digital = localFont({
+  src: '../public/fonts/Digital7-rg1mL.ttf',
+  variable: '--font-digital',
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${digital.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
