47e28101ea4ac497660dba9d30978577717206eb
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-07-03 09:00:08 +0200

    update error handling on server

--- server/operations/division.js
+++ server/operations/division.js
@@ -1,11 +1,5 @@
 function divide(a, b) {
-  if (b === null || b === undefined) return 1;
-  
   if (b === 0) throw new Error('Cannot divide by 0')
-  
-  if (isNaN(a) || isNaN(a)) throw new Error('Parameter is not a number!')
-  
-  if (typeof a === 'string' || typeof b === 'string') throw new Error('Parameter is not a number!')
 
   return a / b;
 }
