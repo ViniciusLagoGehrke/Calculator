5939aa9fed025bf73e5cb7ec3120af9e7ff99783
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-07-03 21:12:22 +0200

    refactor server

--- server/calculator.js
+++ server/calculator.js
@@ -10,23 +10,47 @@ const OPERATIONS = {
   DIVIDE: 'DIVIDE'
 }
 
-function calculator(a, b, operator) {
+const ERROR = {
+  Invalid_Parameter: 'Parameter is not a number!',
+  Invalid_Operator: 'Invalid operator!',
+}
+
+function calculator(number1, number2, operator) {
+  if (number1 === null || number2 === null || number1 === undefined || number2 === undefined) {
+    console.error(ERROR.Invalid_Parameter)
+    throw new Error(ERROR.Invalid_Parameter);
+  }
+
+  if (isNaN(number1) || isNaN(number2)) {
+    console.error(ERROR.Invalid_Parameter)
+    throw new Error(ERROR.Invalid_Parameter);
+  }
+  
+  if (typeof number1 === 'string' || typeof number2 === 'string') {
+    console.error(ERROR.Invalid_Parameter)
+    throw new Error(ERROR.Invalid_Parameter);
+  }
+
+  if(OPERATIONS[operator] === undefined) {
+    console.error(ERROR.Invalid_Operator)
+    throw new Error(ERROR.Invalid_Operator);
+  }
+
   switch (operator) {
     case OPERATIONS.SUM:
-      return sum(a, b)
+      return sum(number1, number2)
 
     case OPERATIONS.SUBTRACT:
-      return subtract(a, b)
+      return subtract(number1, number2)
 
     case OPERATIONS.MULTIPLY:
-      return multiply(a, b)
+      return multiply(number1, number2)
 
     case OPERATIONS.DIVIDE:
-      return divide(a, b)
+      return divide(number1, number2)
   }
 }
 
 module.exports = {
-  calculator,
-  OPERATIONS
+  calculator
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const { calculator } = require('./calculator');
 
 const app = express();
 const PORT = 8080;
@@ -8,39 +9,11 @@ const PORT = 8080;
 app.use(cors());
 app.use(bodyParser.json());
 
-const { calculator, OPERATIONS } = require('./calculator');
-
-const ERROR = {
-  Invalid_Parameter: 'Parameter is not a number!',
-  Invalid_Operator: 'Invalid operator!',
-}
-
 app.post('/calculate', (req, res, next) => {
   try {
     const { number1, number2, operator } = req.body;
 
-    if (number1 === null || number2 === null || number1 === undefined || number2 === undefined) {
-      console.error(ERROR.Invalid_Parameter)
-      return res.status(400).json({ error: ERROR.Invalid_Parameter });
-    }
-
-    if (isNaN(number1) || isNaN(number2)) {
-      console.error(ERROR.Invalid_Parameter)
-      return res.status(400).json({ error: ERROR.Invalid_Parameter });
-    }
-    
-    if (typeof number1 === 'string' || typeof number2 === 'string') {
-      console.error(ERROR.Invalid_Parameter)
-      return res.status(400).json({ error: ERROR.Invalid_Parameter });
-    }
-
-    if(OPERATIONS[operator] === undefined) {
-      console.error(ERROR.Invalid_Operator)
-      return res.status(400).json({ error: ERROR.Invalid_Operator });
-    }
-
     const result = calculator(number1, number2, operator)
-
     return res.json({ result });
 
   } catch (err) {
