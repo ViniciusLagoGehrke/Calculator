1ae99e4f1630686bf4020ca62857502340654193
Vinicius Lago Gehrke <vinictus@gmail.com>
2023-07-03 16:16:04 +0200

    refactor to calculator component

---
+++ client/src/types.ts
@@ -0,0 +1,16 @@
+export const OPERATIONS = {
+  SUM: '+',
+  SUBTRACT: '-',
+  MULTIPLY: '*',
+  DIVIDE: '/'
+} as const
+
+export type OperatorType = typeof OPERATIONS[keyof typeof OPERATIONS]
+
+export const ERROR_MESSAGE = {
+  Invalid_Parameter: 'Parameter is not a number!',
+  Invalid_Operator: 'Invalid operator!',
+  Cannot_Divide_by_0: 'Cannot divide by 0',
+} as const
+
+export type ErrorType = typeof ERROR_MESSAGE[keyof typeof ERROR_MESSAGE]
